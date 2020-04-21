2be26f2d7fe73b5c8ee35e547520451db2d8d312
traxx10 <chuksben16@gmail.com>
2020-04-21 22:23:07 +0100

    added icons and splash screens

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import store from './src/store';
 import {NavigationContainer} from '@react-navigation/native';
 import AppStack from './src/navigation/appStack';
 import {ActivityIndicator} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(props) {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store().store}>
       <PersistGate
